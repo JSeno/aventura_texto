b90320ce2986bfcba3309133af82603ae956607f
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-04-28 00:26:33 -0300

    Fazendo algumas alterações no jogo

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 """
-Um rpg de aventura em texto com uma simples história de um héroi que vai salvar uma princes de um dragão.
-Farei uma pequena alteração no jogo, ele será um aventureiro de uma guilda que deve completar missões dependendo de rank.
+Você é um aventureiro em um mundo de fantasia, para conseguir seu ganha pão você deve trabalhar fazendo missões
+para a guilda de aventureiros.
 
-Os rank serão F,
+Colocarei um sistema de rank conforme você cumpre suas missões. ganha pontos para subir de rank.
+Os rank serão F, E, D, C, B, A, S.
+implementarei um pequeno sistema de batalha, conforme sobe o rank terá missões de batalha.
+sitema de batalha será formado pelas funções:
+destreza x agilidade para acerto e esquiva
+ataque fisíco x defesa fisíca
+ataque magico x defesa magica
 
-
+Estarei usando um atributo fixo dependendo dos atributos dos personagens e um numero aleatório de 1 a 6 similando um dado.
 
 """
-# Importando pygame
-import pygame
-
-# Inicializando o pygame
-pygame.init()
-
+# Herói Clichê v.0.1
 # Tela de título
-pygame.display.set_caption("Heroi cliche")
-
 print('Seja bem vindo ao:')
 print('+-------------------------------------------------+')
 print('|                                                 |')
@@ -24,37 +23,64 @@ print('|                                                 |')
 print('+-------------------------------------------------+')
 print('\n')
 print('Gostaria de jogar?')
-print('\n')
-print('Digite 1 para sim e 2 para não')
-print('\n')
+print('Por favor digite sim ou não')
 
-primeira_resposta = int(input())
-if primeira_resposta == 1:
-    print('Vamos começar!')
+enter_play = input('Digite: ')
+if enter_play == 'sim' or enter_play == 'Sim' or enter_play == 's' or enter_play == 'S':
+    print('Ok, vamos começar!')
 else:
     print('Tchau!')
     exit()
 
-print('Qual o seu nome?')
-nome = input()
-print('Olá, ' + nome + '!')
+name_hero = input('Por favor me diga qual é seu nome aventureiro: ')
+print(f'Prazer em te conhecer {name_hero}')
+print('\n')
+print('Então vamos começar a sua aventura!')
+print('\n')
+print('Qual tipo de personagem você é?')
+print('Por acaso um tipo guerreiro, um tipo mago ou um tipo ladino?')
+print('\n')
 
-print('Você é um cavaleiro, um arqueiro ou um mago?')
-print('Digite 1 para cavaleiro, 2 para arqueiro ou 3 para mago')
-
-classe_escolhida = int(input())
-if classe_escolhida == 1:
-    print('Você escolheu a classe cavaleiro!')
-elif classe_escolhida == 2:
-    print('Você escolheu a classe arqueiro!')
-elif classe_escolhida == 3:
-    print('Você escolheu a classe mago!')
+# TODO: colocar escolha de job como dicionário e escolher pelo .values() e .keys()
+job_hero_choice = input('Digite: ')
+if job_hero_choice == 'guerreiro' or job_hero_choice == 'Guerreiro' or job_hero_choice == 'g' or job_hero_choice == 'G':
+    job_hero_choice = 'guerreiro'
+    print('Então você é um guerreiro, gosta de ir com tudo esmagando seus inimigos!')
+elif job_hero_choice == 'mago' or job_hero_choice == 'Mago' or job_hero_choice == 'm' or job_hero_choice == 'M':
+    job_hero_choice = 'mago'
+    print('Então você é um mago, gosta de seus poderes mágicos!')
+elif job_hero_choice == 'ladino' or job_hero_choice == 'Ladino' or job_hero_choice == 'l' or job_hero_choice == 'L':
+    job_hero_choice = 'ladino'
+    print('Então você é um ladino, gosta de correr atrás de tesouros e andar silenciosamente!')
 else:
-    print('Você não escolheu nenhuma classe!')
+    print('Você não é um personagem válido!')
     exit()
 
+# TODO: o copilot ajudou no dicionário mas não são todos atributos do jeito que quero pensarei e farei uma correção
+# Atributos base do personagem
+hero_b_atributos = {
+        'nome': name_hero,
+        'classe': job_hero_choice,
+        'vida': 100,
+        'mana': 100,
+        'força': 10,
+        'destreza': 10,
+        'agilidade': 10,
+        'inteligência': 10,
+        'resistencia': 10,
+}
 
-
-
-
-
+# TODO: mudarei o formato das classes com dicionário ai dependendo da classe será acrestentado ou retirado atributos.
+# Caso seu personagem seja um guerreiro
+hero_c_guerreiro = {
+        'nome': name_hero,
+        'classe': job_hero_choice[0],
+        'vida': hero_b_atributos['vida'] + 25,
+        'mana': hero_b_atributos['mana'] / 3,
+        'força': hero_b_atributos['força'] + 5,
+        'destreza': hero_b_atributos['destreza'],
+        'agilidade': hero_b_atributos['agilidade'],
+        'inteligência': hero_b_atributos['inteligência'] - 5,
+        'resistencia': hero_b_atributos['resistencia'] + 5,
+}
+print(hero_c_guerreiro.values())
