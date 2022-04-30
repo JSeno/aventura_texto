eb8c434e2bac524a6a0a1da8bf854e531ef19dcb
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-04-30 17:36:30 -0300

    Atualização do jogo, para somente jogo de escolhas

--- main.py
+++ main.py
@@ -32,55 +32,56 @@ else:
     print('Tchau!')
     exit()
 
-name_hero = input('Por favor me diga qual é seu nome aventureiro: ')
-print(f'Prazer em te conhecer {name_hero}')
-print('\n')
-print('Então vamos começar a sua aventura!')
-print('\n')
-print('Qual tipo de personagem você é?')
-print('Por acaso um tipo guerreiro, um tipo mago ou um tipo ladino?')
-print('\n')
 
-# TODO: colocar escolha de job como dicionário e escolher pelo .values() e .keys()
-job_hero_choice = input('Digite: ')
-if job_hero_choice == 'guerreiro' or job_hero_choice == 'Guerreiro' or job_hero_choice == 'g' or job_hero_choice == 'G':
-    job_hero_choice = 'guerreiro'
-    print('Então você é um guerreiro, gosta de ir com tudo esmagando seus inimigos!')
-elif job_hero_choice == 'mago' or job_hero_choice == 'Mago' or job_hero_choice == 'm' or job_hero_choice == 'M':
-    job_hero_choice = 'mago'
-    print('Então você é um mago, gosta de seus poderes mágicos!')
-elif job_hero_choice == 'ladino' or job_hero_choice == 'Ladino' or job_hero_choice == 'l' or job_hero_choice == 'L':
-    job_hero_choice = 'ladino'
-    print('Então você é um ladino, gosta de correr atrás de tesouros e andar silenciosamente!')
+name_hero = input('Por favor me diga qual é seu nome aventureiro:\n ')
+hero_class = {1: 'Guerreiro', 2: 'Mago', 3: 'Arqueiro'}
+
+print('Posso saber qual é sua classe de personagem?')
+hero_class_choice = input('Digite 1 para Guerreiro, 2 para Mago e 3 para Arqueiro: ')
+if hero_class_choice == '1':
+    hero_class_choice = hero_class[1]
+elif hero_class_choice == '2':
+    hero_class_choice = hero_class[2]
+elif hero_class_choice == '3':
+    hero_class_choice = hero_class[3]
 else:
-    print('Você não é um personagem válido!')
+    print('Você não escolheu uma classe válida, vamos começar de novo')
     exit()
+print('Vamos confirmar seu registro então, seu nome é: ' + name_hero.title() + ' e sua classe é: ' + hero_class_choice)
+print('Como você está começando agora, você pertence ao RANK: F')
+print('Só posso te passar pequenas missões dependendo de seu rank, quando você completar 10 missões do seu rank, você subirá de rank')
+print('Vamos começar!')
+print('\n')
 
-# TODO: o copilot ajudou no dicionário mas não são todos atributos do jeito que quero pensarei e farei uma correção
-# Atributos base do personagem
-hero_b_atributos = {
-        'nome': name_hero,
-        'classe': job_hero_choice,
-        'vida': 100,
-        'mana': 100,
-        'força': 10,
-        'destreza': 10,
-        'agilidade': 10,
-        'inteligência': 10,
-        'resistencia': 10,
-}
+# Missões Rank F
+rank_f = 0
 
-# TODO: mudarei o formato das classes com dicionário ai dependendo da classe será acrestentado ou retirado atributos.
-# Caso seu personagem seja um guerreiro
-hero_c_guerreiro = {
-        'nome': name_hero,
-        'classe': job_hero_choice[0],
-        'vida': hero_b_atributos['vida'] + 25,
-        'mana': hero_b_atributos['mana'] / 3,
-        'força': hero_b_atributos['força'] + 5,
-        'destreza': hero_b_atributos['destreza'],
-        'agilidade': hero_b_atributos['agilidade'],
-        'inteligência': hero_b_atributos['inteligência'] - 5,
-        'resistencia': hero_b_atributos['resistencia'] + 5,
-}
-print(hero_c_guerreiro.values())
+missao_rank_f = {1: 'Coletar 10 folhas de cinauro',
+                 2: 'Uma criança de um nobre perdeu seu animal de estimação, você deve encontra-lo',
+                 3: 'A igreja do deus do Fogo da Luz precisa de alguns reparos em seu telhado, você precisa consertar',
+                 4: 'Verficar o que está ocorrendo com o Poço em frente ao mercado, nele está vindo um mal cheiro'
+                 }
+
+print('Qual dessas missões você gostaria de fazer?')
+print('1 - ' + missao_rank_f[1])
+print('2 - ' + missao_rank_f[2])
+print('3 - ' + missao_rank_f[3])
+print('4 - ' + missao_rank_f[4])
+print('Hoje somente temos essas 4 missões')
+missao_rank_f_choice = input('Digite 1, 2, 3 ou 4: ')
+if missao_rank_f_choice == '1':
+    missao_rank_f_choice = missao_rank_f[1]
+elif missao_rank_f_choice == '2':
+    missao_rank_f_choice = missao_rank_f[2]
+elif missao_rank_f_choice == '3':
+    missao_rank_f_choice = missao_rank_f[3]
+elif missao_rank_f_choice == '4':
+    missao_rank_f_choice = missao_rank_f[4]
+else:
+    print('Você não escolheu uma missão válida, vamos começar de novo')
+    exit()
+print('Você escolheu a missão: ' + missao_rank_f_choice)
+print('\n')
+
+
+
